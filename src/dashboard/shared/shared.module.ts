6da702394a7781bfbfc281e433af5f93ec699606
anonymoussc <50c5ac69@opayq.com>
2019-02-08 02:01:16 +0700

    feat(module): add dashboard/shared

---
+++ src/dashboard/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { SharedRoutingModule } from './shared-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, SharedRoutingModule]
+})
+export class SharedModule {}
