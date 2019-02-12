c2a651baa65d301e718fa61f40061a50cef57013
anonymoussc <50c5ac69@opayq.com>
2019-02-12 12:57:48 +0700

    refactor(module): update dashboard login mod

--- src/dashboard/login/pages/login-page/login-page.component.ts
+++ src/dashboard/login/pages/login-page/login-page.component.ts
@@ -1,12 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@ngx-auth/core';
+import { TranslateService } from '@ngx-translate/core';
+import { from as observableFrom, Observable } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { BaseComponent } from '~/@enoct/framework/core';
+import { routeAnimation } from '~/@enoct/shared';
 
 @Component({
-  selector   : 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls  : ['./login-page.component.scss']
+  selector       : 'app-login-page',
+  templateUrl    : './login-page.component.html',
+  styleUrls      : ['./login-page.component.scss'],
+  animations     : [routeAnimation],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginPageComponent implements OnInit {
-  constructor() {}
+export class LoginPageComponent extends BaseComponent implements OnInit {
+  username: string;
+  password: string;
+  isProcessing: boolean;
+  note$: Observable<string>;
+  error$: Observable<string>;
 
-  ngOnInit() {}
+  constructor(
+    private readonly auth: AuthService,
+    private readonly translate: TranslateService,
+    private readonly router: Router
+  ) {
+    super();
+  }
+
+  ngOnInit(): void {
+    if (this.auth.isAuthenticated) {
+      observableFrom(this.router.navigateByUrl(this.auth.defaultUrl))
+        .pipe(takeUntil(this.ngUnsubscribe))
+        .subscribe(() => {
+          /**/
+        });
+    }
+  }
+
+  login(): Observable<boolean> {
+    this.isProcessing = true;
+    this.note$        = this.translate.get('PUBLIC.LOGIN.NOTE');
+
+    const auth$ = this.auth.authenticate(this.username, this.password).pipe(takeUntil(this.ngUnsubscribe));
+
+    auth$.subscribe(() => {
+      this.isProcessing = false;
+
+      if (!this.auth.isAuthenticated) {
+        this.error$ = this.translate.get('PUBLIC.LOGIN.ERROR');
+      }
+    });
+
+    return auth$;
+  }
 }
