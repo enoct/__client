0633da404f2480571060553196221d73757d62d5
anonymoussc <50c5ac69@opayq.com>
2019-02-14 11:32:08 +0700

    refactor(module): modified app.browser.module.ts
    Signed-off-by: anonymoussc <50c5ac69@opayq.com>

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,3 +1,13 @@
+/**
+ * app.browser.module.ts
+ * Created by @anonymoussc on 02/14/2019 12:02 AM.
+ */
+
+/*
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 2/14/19 12:04 AM
+ */
+
 import { NgModule } from '@angular/core';
 import { BrowserTransferStateModule, TransferState } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -6,8 +16,8 @@ import { AuthModule } from '@ngx-auth/core';
 import { CACHE } from '@ngx-cache/core';
 import { BrowserCacheModule, MemoryCacheService } from '@ngx-cache/platform-browser';
 import 'hammerjs';
-import { AuthTestingModule } from '~/app/framework/auth/testing';
-import { ConsoleService, CoreModule, WindowService } from '~/app/framework/core';
+import { AuthTestingModule } from '~/@enoct/framework/auth/testing';
+import { ConsoleService, CoreModule, WindowService } from '~/@enoct/framework/core';
 
 import { AppComponent } from './app.component';
 import { AppModule, REQ_KEY } from './app.module';
