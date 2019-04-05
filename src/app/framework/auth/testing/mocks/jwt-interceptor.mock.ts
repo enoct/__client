08a544faf9471db6aae97e71ed1b7382a5659218
anonymoussc <50c5ac69@opayq.com>
2019-04-05 08:19:30 +0700

    refactor(app): update app auth

--- src/app/framework/auth/testing/mocks/jwt-interceptor.mock.ts
+++ src/app/framework/auth/testing/mocks/jwt-interceptor.mock.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AuthLoader } from '@ngx-auth/core';
+import { AuthLoader } from '~/@enoct/framework/auth';
 import { flow, get } from 'lodash/fp';
 import { Observable } from 'rxjs';
 
